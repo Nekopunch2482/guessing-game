01462223c9b5887fd548f094e39d33f3ea7659fd
Nekopunch <Nekopunch2482@yandex.ru>
2024-09-02 13:37:32 +0300

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.left = 0;
+    this.right = 0;
+    this.prev_guess = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.left = min;
+    this.right = max;
+  }
 
-    }
+  guess() {
+    this.prev_guess = Math.ceil((this.left + this.right) / 2);
+    return this.prev_guess;
+  }
 
-    guess() {
+  lower() {
+    this.right = this.prev_guess;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.left = this.prev_guess;
+  }
 }
 
 module.exports = GuessingGame;
